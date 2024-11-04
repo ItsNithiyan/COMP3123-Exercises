a590b1c42baa7eafeb96ad53330582cae73003e2
Nithiyan Annapoorani Valluvan <itsnithiyan@gmail.com>
2024-11-03 23:08:28 -0500

    Initial commit for lab week 09

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Details from "./studentDetails";
+import Head from "./studentDetails";
+
+function App() {
+  const studentId = "101418712";
+  const name = "Nithiyan Annapoorani Valluvan";
+  const college = "George Brown College, Toronto";
+  const mHead = "Welcome to Fullstack Developmet - I";
+  const sHead = "React JS Programming Week09 Lab Exercise";
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <Details
+          mainHead={mHead}
+          subHead={sHead}
+          studentId={studentId}
+          studentName={name}
+          studentClg={college}
+        />
+      </header>
+    </div>
+  );
+}
+
+export default App;
